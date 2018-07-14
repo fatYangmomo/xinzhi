bdde0281b1d79b8d5046c2bb7446e6a06721498a
bwlu <18232386357@163.com>
2018-07-14 13:49:02 +0800

    股份列表下载

--- main/views/download.py
+++ main/views/download.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.views import View
-import urllib.request
+import urllib.request as urllib2
 from ..utils import (
     correct_response, err_response_with_desc
 )
@@ -17,15 +17,51 @@ class DownloadStock(View):
 		url3 = 'http://www.csindex.com.cn/uploads/file/autofile/cons/000016cons.xls'
 		# 沪深300成份股列表地址xls
 		url4 = 'http://www.csindex.com.cn/uploads/file/autofile/cons/000300cons.xls'
-		# 历史交易数据表csv
-		url5 = 'http://quotes.money.163.com/service/chddata.html?code=1002639&start=20171118&end=20180711&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;TURNOVER;VOTURNOVER;VATURNOVER;TCAP;MCAP' 
-		# 资产负债表xls
-		url6 = 'http://money.finance.sina.com.cn/corp/go.php/vDOWN_BalanceSheet/displaytype/4/stockid/000001/ctrl/all.phtml'
-		# 利润表xls
-		url7 = 'http://money.finance.sina.com.cn/corp/go.php/vDOWN_ProfitStatement/displaytype/4/stockid/000002/ctrl/all.phtml'
-		f = urllib.request.urlopen(url1) 
+		send_headers = {
+			'User-Agent':'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299',
+			'Referer':'http://www.sse.com.cn/assortment/stock/list/share/',
+		}
+		req = urllib2.Request(url2, headers=send_headers)
+		f = urllib2.urlopen(req)
 		data = f.read()
-		with open("download/file_stock/深交所A股股票上市公司列表.xlsx", "wb") as code:
+		with open("download/file_stock/上交所A股上市公司列表.xls", "wb") as code:
 			code.write(data)
-		return correct_response({})
+		urls = [
+			{"url":url1,"name":"深交所A股上市公司列表.xlsx"},
+			{"url":url3,"name":"上证50成份股列表.xls"},
+			{"url":url4,"name":"沪深300成份股列表.xls"}
+		]
+		for url in urls:
+			f = urllib2.urlopen(url["url"])
+			file_name = url["name"]
+			data = f.read()
+			with open("download/file_stock/"+file_name, "wb") as code:
+				code.write(data)
+		return correct_response({'data':'股份列表下载成功'})
+
+# class DownloadReport(View):
+	
+# 	def get(self,request):
+# 		# 历史交易数据表csv
+# 		url5 = 'http://quotes.money.163.com/service/chddata.html?code=1002639&start=20171118&end=20180711&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;TURNOVER;VOTURNOVER;VATURNOVER;TCAP;MCAP' 
+# 		# 资产负债表xls-txt
+# 		url6 = 'http://money.finance.sina.com.cn/corp/go.php/vDOWN_BalanceSheet/displaytype/4/stockid/000001/ctrl/all.phtml'
+# 		# 利润表xls-txt
+# 		url7 = 'http://money.finance.sina.com.cn/corp/go.php/vDOWN_ProfitStatement/displaytype/4/stockid/000002/ctrl/all.phtml'
+# 		send_headers = {
+# 			'User-Agent':'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299',
+# 			'Referer':'http://www.sse.com.cn/assortment/stock/list/share/',
+# 		}
+# 		urls = [
+# 			{"url":url5,"name":"历史交易数据表.csv"},
+# 			{"url":url6,"name":"资产负债表.xls"},
+# 			{"url":url7,"name":"利润表.xls"}
+# 		]
+# 		for url in urls:
+# 			f = urllib2.urlopen(url["url"])
+# 			file_name = url["name"]
+# 			data = f.read()
+# 			with open("download/file_report/"+file_name, "wb") as code:
+# 				code.write(data)
+# 		return correct_response({})
 		
